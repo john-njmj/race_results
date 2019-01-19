d358a15a857708f2d0730a3ef1abaedadd6c4408
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-01-19 10:46:13 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -104,7 +104,7 @@ local function update_parameter(par_name,par_val)
    elseif par_name == "result_files" then       
       result_files ={} 
       for idx = 1, #par_val do
-          result_files[idx]=par_val[idx]    
+          result_files[idx]=par_val.filename    
       end     
    else
       print ("unknown PARAMETER :", par_name)
