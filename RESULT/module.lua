38a1d5f6b81096d7ac93684f6bb95081b1705e08
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-01-31 13:19:34 +0100

    Update module.lua

--- RESULT/module.lua
+++ RESULT/module.lua
@@ -150,7 +150,7 @@ end
 function M.draw()
 
    if result_mode == "RESULT" then
-      result_logo:draw(0,scroller_size, screen_width,screen_height)
+      result_logo:draw(screen_width - 217,screen_height-207, screen_width,screen_height)
       V_pos = scroller_size + 10
       for r,result in ipairs(results) do 
          if result ~= "Null" then 
