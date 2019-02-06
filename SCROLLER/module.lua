27beb2279a7344a37d5e2b158b78c312ced986fa
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 16:46:17 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -82,8 +82,9 @@ function draw_scroller()
    -- keep looping over the texts until textend is offscreen or there is nothing do display
 	repeat
 	   for idx , text_line in ipairs(texts) do
-		  scroller_font:write(0, idx * 200, text_line.t_active, scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
-			if text_line.t_active ~= "N" then 
+		scroller_font:write(0, idx * 100, text_line.t_active, scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
+		if text_line.t_active ~= "N" then 
+		  scroller_font:write(200, idx * 100, text_line.t_active, scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
 		  textend = textstart + text_line.t_width
 		  if (textstart < scroller_offset and textend > scroller_offset) or (textstart >= scroller_offset and textstart <= scroller_offset +screen_width) then
 		     text_line.b_image:draw(textstart - scroller_offset, 0,textend - scroller_offset,scroller_size,1)
