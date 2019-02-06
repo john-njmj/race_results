87ba49b9014dce9408a8fa03313ab489a0a374c3
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 14:49:51 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -1,17 +1,13 @@
+print ("### Scroller INIT")
 local localized, CHILDS, CONTENTS = ...
-
 local M = {}
 local texts ={}
-local scroller_len 
-local text
-local scroller_collor
+local scroller_len = 0
 local json = require "json"
-local last_time = sys.now()
-
-
-print ("### Scroller INIT")
 local scroller_speed = - 10 -- default scroller_speed
 local scroller_font = resource.load_font(localized("font.ttf")) -- default font 
+--local last_time = sys.now()
+local scroller_pos  = 0
 
 local function load_config(raw)
     -- proccess the config file 
@@ -65,26 +61,27 @@ function M.content_remove(name)
 end
 
 function draw_scroller()
-   -- move scroller_pos to next postion 
-   local curent_time = sys.now()
+   -- calculate scroller_pos relative to the time 
+   if scroller_len ~= 0 then 
+   	scroller_pos = (sys.now() * scroller_speed) % scroller_len
+   else
+	scroller_pos = 0	
+   end 
    local textstart = 0
    local textend = 0
-   scroller_pos = scroller_pos + ((curent_time-last_time) * scroller_speed)
-   last_time = curent_time
    -- rest the postion if we are at a full lengt away from 0
-   if math.abs(scroller_pos) > scroller_len then
-	scroller_pos = 0 
-   end 
+--   if math.abs(scroller_pos) > scroller_len then
+--	scroller_pos = 0 
+--   end 
    -- start at the most left position  
    if scroller_pos <= 0 then 
 	textstart = scroller_pos 
    else 
 	textstart = scroller_pos - scroller_len 
    end  
-   -- keep loping over the texts until textend is offscreen
+   -- keep looping over the texts until textend is offscreen
 	repeat
 	   for idx , text_line in ipairs(texts) do
-		--loop over texts
 		textend = textstart + text_line.t_width
 		if (textstart < scroller_offset and textend > scroller_offset) or (textstart >= scroller_offset and textstart <= scroller_offset +screen_width) then
 		   text_line.b_image:draw(textstart - scroller_offset, 0,textend - scroller_offset,scroller_size,1)
@@ -94,27 +91,6 @@ function draw_scroller()
 		-- breack if textstart is off screen 
 	   end
 	until textend > scroller_offset + screen_width
---OLD code 		
---	--check if text will be on screen for current position 
---   offscreen_pos_left =  0 - scroller_text_len - scroller_space_len
---   --reset to most left position  
---   while scroller_pos > offscreen_pos_left do
---      scroller_pos = scroller_pos - scroller_text_len - scroller_space_len
---  end
---   -- correct calculation, while always ends a fulltext to far to the left
---   scroller_pos = scroller_pos + scroller_text_len + scroller_space_len
---   -- write text and repeat writing until we are a the end of the screen  
---   draw_pos = scroller_pos 
---   while draw_pos < scroller_offset - scroller_text_len - scroller_space_len do
---      draw_pos = draw_pos + scroller_text_len + scroller_space_len
---   end
---   draw_pos = draw_pos - scroller_offset
---	repeat
---	  scroller_font:write(draw_pos, 0, scroller_text .. scroller_space, scroller_size, text_color.r,text_color.g,text_color.b,text_color.a)
---      draw_pos = draw_pos + scroller_text_len + scroller_space_len
---	until draw_pos > scroller_offset + screen_width
- 
- 
 end 
 
 
@@ -125,5 +101,4 @@ function M.draw()
       scroller_font:write(0, 0, "MY IP", scroller_size, 1,1,0,1)
    end 
 end
-
 return M
