8718bd3a9d1d2d2f4f5f3c1e7407c21d585f1160
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 10:25:09 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -22,15 +22,21 @@ local function load_config(raw)
 	scroller_len = 0
 	for idx = 1 , #config.scroller_text_list do
 		new_text=config.scroller_text_list[idx]
+		print ("## new_text ## " .. new_text.s_text)
 		new_text.t_width = scroller_font:width(new_text.s_text,scroller_size)
 		new_text.b_image = resource.create_colored_texture(new_text.b_color.r, new_text.b_color.g, new_text.b_color.b, new_text.b_color.a)
 		-- separator has same parameters as text -> copy and update text and width
+		print ("## new_text ## " .. new_text.s_text)
 		new_sep = new_text
+		print ("## new_text ## " .. new_text.s_text)
 		new_sep.s_text = config.scroller_space
+		print ("## new_text ## " .. new_text.s_text)
 		new_sep.t_width = scroller_font:width(new_sep.s_text,scroller_size)
-		
+		print ("## new_text ## " .. new_text.s_text)
 		texts[((idx*2)-1)] = new_text
+		print ("## new_text ## " .. new_text.s_text)
 		texts[(idx*2)] = new_sep
+		print ("## new_text ## " .. new_text.s_text)
 		scroller_len = scroller_len + new_text.t_width + new_sep.t_width
 		print ("## new_text ## " .. new_text.s_text)
 		print ("## new_sep ## " .. new_sep.s_text)
