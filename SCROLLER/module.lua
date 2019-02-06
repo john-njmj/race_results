3583d671cd636ab1a6e26304241c4ce7d6950da0
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 16:58:19 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -86,6 +86,7 @@ function draw_scroller()
 		  textend = textstart + text_line.t_width
 		  if (textstart < scroller_offset and textend > scroller_offset) or (textstart >= scroller_offset and textstart <= scroller_offset +screen_width) then
 		     text_line.b_image:draw(textstart - scroller_offset, 0,textend - scroller_offset,scroller_size,1)
+		     scroller_font:write(0, 200, sys.now().." "..math.floor(sys.now() / 2), scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
 		     if text_line.t_active == "O" or math.floor(sys.now() / 2) == 0 then
 		     	scroller_font:write(textstart - scroller_offset, 0, text_line.s_text, scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
 		     end
