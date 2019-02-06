d04a2a6fe1411ccd6aab7e31843de2eac6986dc6
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 17:50:15 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -91,9 +91,9 @@ function draw_scroller()
 		  textend = textstart + text_line.t_width
 		  if (textstart < scroller_offset and textend > scroller_offset) or (textstart >= scroller_offset and textstart <= scroller_offset +screen_width) then
 		     text_line.b_image:draw(textstart - scroller_offset, 0,textend - scroller_offset,scroller_size,1)
-		     --if text_line.t_active == "O" or (math.floor(sys.now()) % 2) == 0 then
+		     if text_line.t_active == "O" or (math.floor(sys.now()) % 2) == 0 then
 		     	scroller_font:write(textstart - scroller_offset, 0, text_line.s_text, scroller_size, text_line.t_color.r,text_line.t_color.g,text_line.t_color.b,text_line.t_color.a)
-		     --end
+		     end
 		  end				
 		  textstart = textend
 		  -- Can be optimized with : breack if textstart is off screen on right 
