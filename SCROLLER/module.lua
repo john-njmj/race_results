91ee5304cabcc5b8a3b6758c0a5f4acc25671320
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-06 11:24:44 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -19,6 +19,7 @@ local function load_config(raw)
 	texts = {}
 	scroller_len = 0
 	for idx = 1 , #config.scroller_text_list do
+		print ("type config.scroller_text_list[] : ".. type(onfig.scroller_text_list[idx]))
 		local new_text=config.scroller_text_list[idx]
 		print ("## new_text 1## " .. new_text.s_text)
 		new_text.t_width = scroller_font:width(new_text.s_text,scroller_size)
