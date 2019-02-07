f05beb991962b3f8c554c37d9149ceadb60e7b18
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-07 13:02:51 +0100

    Update module.lua

--- SCROLLER/module.lua
+++ SCROLLER/module.lua
@@ -67,7 +67,6 @@ function draw_scroller()
    else
 	scroller_pos = 0	
    end
-scroller_font:write(0, 200, scroller_pos, scroller_size,1,1,1,1)	
    local textstart = 0
    local textend = 0
    -- rest the postion if we are at a full lengt away from 0
@@ -80,7 +79,6 @@ scroller_font:write(0, 200, scroller_pos, scroller_size,1,1,1,1)
    else 
 	textstart = scroller_pos - scroller_len 
    end
-   scroller_font:write(0, 400, textstart, scroller_size,1,1,1,1)
    -- keep looping over the texts until textend is offscreen or there is nothing do display
 	repeat
 	   for idx , text_line in ipairs(texts) do
